97e7fc8e6011bea63da6790e44bdcf6628de2ea0
ayoubidriss <106492025+ayoubidriss@users.noreply.github.com>
2023-12-03 23:07:22 +0100

    Update 2_🪟_Split_Map.py

--- pages/2_🪟_Split_Map.py
+++ pages/2_🪟_Split_Map.py
@@ -1,26 +1,8 @@
-import streamlit as st
+import folium
 import leafmap.foliumap as leafmap
 
-st.set_page_config(layout="wide")
-
-markdown = """
-Web App URL: <https://geotemplate.streamlit.app>
-GitHub Repository: <https://github.com/giswqs/streamlit-multipage-template>
-"""
-
-st.sidebar.title("About")
-st.sidebar.info(markdown)
-logo = "https://i.imgur.com/UbOXYAU.png"
-st.sidebar.image(logo)
-
-st.title("Split-panel Map")
-
-with st.expander("See source code"):
-    with st.echo():
-        m = leafmap.Map()
-        m.split_map(
-            left_layer='ESA WorldCover 2020 S2 FCC', right_layer='ESA WorldCover 2020'
-        )
-        m.add_legend(title='ESA Land Cover', builtin_legend='ESA_WorldCover')
-
+m = leafmap.Map(height=600, center=[39.4948, -108.5492], zoom=12)
+url = 'Attibut1Jour-1.tif'
+url2 = 'Attibut1Jour0.tif'
+m.split_map(url, url2)
 m.to_streamlit(height=700)
